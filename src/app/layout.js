0e9e5b6abb4d55a2af42f0995404ca0c7d310306
erhantezer <erhantezer68@gmail.com>
2025-08-16 13:15:24 +0200

    google analytics added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,31 @@
-import '../app/globals.css'; // ✅ doğru yol
+// app/layout.js
+import "../app/globals.css";
+import Script from "next/script";
 
 export const metadata = {
-  title: 'Flashcards',
-  description: 'Norsk verbøvelse',
+  title: "Flashcards",
+  description: "Norsk verbøvelse",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="no">
       <head>
-        
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-RE2JQ4BPWB"
+        />
+        <Script id="ga" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            window.gtag = gtag;
+            gtag('js', new Date());
+            gtag('config', 'G-RE2JQ4BPWB');
+          `}
+        </Script>
       </head>
       <body>{children}</body>
     </html>
   );
 }
-
